015ff7ce729045c42bc84a6f1c4e7cc4c4dd714d
Elton Chou <plscd748@gmail.com>
2021-12-11 16:22:13 +0000

    refine: refined start script

--- hook_crawlers/scrapyd_start.sh
+++ hook_crawlers/scrapyd_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COMMAND=$1
 EGGS_DIR=eggs
 
 # set proxy-list path
@@ -6,20 +7,54 @@ export PROXY_LIST=$(readlink -f $(find . -name proxy-list.txt))
 
 # clean old stuffs
 
-if [ -d "$EGGS_DIR" ]; then
-    rm -r $EGGS_DIR/$p
+if [ -z "$COMMAND" ]; then
+    echo "Please specified command 'run' or 'test'."
+    exit 1
 fi
 
-SPIDER_PROJECTS=$(python _cmd.py get-project)
-for p in $SPIDER_PROJECTS;
-    do
-        mkdir -p $EGGS_DIR/$p
-        scrapyd-deploy --build-egg $EGGS_DIR/$p/$(date +%s).egg
-    done
+clean_old () {
+    echo "Clean old egg in $EGGS_DIR"
+    if [ -d "$EGGS_DIR" ]; then
+        rm -r $EGGS_DIR/$p
+    fi
+}
 
-if python _cmd.py checkdb; then
-    scrapyd
+get_project () {
+    python _cmd.py get-project
+}
+
+check_db () {
+    python _cmd.py checkdb
+}
+
+make_egg () {
+    SPIDER_PROJECTS=$(get_project)
+
+    for p in $SPIDER_PROJECTS;
+        do
+            mkdir -p $EGGS_DIR/$p
+            scrapyd-deploy --build-egg $EGGS_DIR/$p/$(date +%s).egg
+        done
+}
+
+
+# pre-process
+clean_old
+make_egg
+
+
+if [ "$COMMAND" == "test" ]; then
+    nohup scrapyd &
+    sleep 5s
+    pkill scrapyd
+elif [ "$COMMAND" == "run" ]; then
+    if check_db; then
+        scrapyd
+    else
+        echo "Can't get connection with db."
+        exit 1
+    fi
 else
-    echo "Can't get connection with db."
+    echo "Please specified command 'run' or 'test'."
     exit 1
 fi
