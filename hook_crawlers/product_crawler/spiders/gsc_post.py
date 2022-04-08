348623de867cc1f35a6421ecd4b637e3b667f16a
Elton Chou <plscd748@gmail.com>
2022-04-08 09:51:38 +0000

    add: add gsc new delay post spider

--- hook_crawlers/product_crawler/spiders/gsc_post.py
+++ hook_crawlers/product_crawler/spiders/gsc_post.py
@@ -1,6 +1,7 @@
 import re
 import urllib.parse
-from typing import Iterable, Optional, Set
+from abc import ABC
+from typing import Iterable, Literal, Optional
 
 import scrapy
 from bs4 import BeautifulSoup
@@ -14,12 +15,12 @@ from scrapy.spiders import CrawlSpider, Rule
 from sqlalchemy import and_, select
 
 
-class GscDelayPostSpider(CrawlSpider):
-    name = 'gsc_delay_post'
+class GscDelayPostAbstractSpider(CrawlSpider, ABC):
     allowed_domains = [BrandHost.GSC]
     start_urls = [
         'https://www.goodsmile.info/ja/posts/category/information/date/'
     ]
+
     rules = [
         Rule(
             LinkExtractor(
@@ -35,55 +36,15 @@ class GscDelayPostSpider(CrawlSpider):
         )
     ]
 
-    def __init__(self, begin_year: Optional[int] = None, end_year: Optional[int] = None, *args, **kwargs):
-        year_pattern = r"\d+"
-        if begin_year and end_year:
-            year_pattern = range_to_regex(int(begin_year), int(end_year))
-
-        rules = list(self.rules)
-        rules.append(
-            Rule(
-                LinkExtractor(
-                    allow=rf'https://.*\.?goodsmile.info/ja/posts/category/information/date/(year_pattern)$',
-                    unique=True
-                ),
-            ),
-        )
-
-        self.rules = rules
-        super().__init__(*args, **kwargs)
-
     def parse_delay_post(self, response):
-        products_delayed = {}
-        product_delayed_ids: Set[str] = set()
         page = BeautifulSoup(response.text, 'lxml')
-
-        for e in page.findAll('br'):
-            e.extract()
-
-        changed_ps = page.select(
-            ".content > p, .content > center", string=re.compile("発売[月|日]変更")
-        )
-        for c in changed_ps:
-            if 'JAN' in c.get_text():
-                results = re.findall(
-                    r"「.*href=\"(.*\/product\/(\d+)\/.*)\".*」JAN：(\d+)", str(c)
-                )
-                for r in results:
-                    url = r[0]
-                    maker_id = r[1]
-                    jan = r[2]
-                    products_delayed[maker_id] = {
-                        'url': url,
-                        'jan': jan
-                    }
-                    product_delayed_ids.add(maker_id)
+        products_delayed = _parse_delay_products_from_post(page)
 
         products_recorded_in_db = fetch_gsc_products_by_official_id(
-            product_delayed_ids
+            products_delayed.keys()
         )
 
-        for p_id in product_delayed_ids & products_recorded_in_db:
+        for p_id in set(products_delayed) & products_recorded_in_db:
             yield scrapy.Request(
                 RelativeUrl.gsc(products_delayed[p_id]['url']),
                 callback=self.parse_product,
@@ -104,6 +65,45 @@ class GscDelayPostSpider(CrawlSpider):
         )
         product.jan = jan
         yield product
+
+    def add_rule(self, rule: Rule):
+        rules = list(self.rules)
+        rules.append(rule)
+        self.rules = rules
+
+
+class GscNewDelayPostSpider(GscDelayPostAbstractSpider):
+    name = 'gsc_new_delay_post'
+
+    def parse(self, response, **kwargs):
+        page = BeautifulSoup(response.text, 'lxml')
+        new_post_links = [
+            RelativeUrl.gsc(icon.parent.get('href'))
+            for icon in page.select(".newsS > a > .newicon")
+            if icon.parent
+        ]
+        for link in new_post_links:
+            yield scrapy.Request(link)
+
+
+class GscDelayPostSpider(GscDelayPostAbstractSpider):
+    name = 'gsc_delay_post'
+
+    def __init__(self, begin_year: Optional[int] = None, end_year: Optional[int] = None, *args, **kwargs):
+        year_pattern = r"\d+"
+        if begin_year and end_year:
+            year_pattern = range_to_regex(int(begin_year), int(end_year))
+
+        self.add_rule(
+            Rule(
+                LinkExtractor(
+                    allow=fr'https://.*\.?goodsmile.info/ja/posts/category/information/date/{year_pattern}$',
+                    unique=True,
+                ),
+            ),
+        )
+
+        super().__init__(*args, **kwargs)
 
 
 def fetch_gsc_products_by_official_id(ids: Iterable) -> set[str]:
@@ -130,3 +130,32 @@ def fetch_gsc_products_by_official_id(ids: Iterable) -> set[str]:
 def range_to_regex(begin: int, end: int):
     range_pattern = r"|".join([str(num) for num in range(begin, end+1)])
     return f"({range_pattern})"
+
+
+DelayTag = dict[str, dict[Literal['jan', 'url'], str]]
+
+
+def _parse_delay_products_from_post(page: BeautifulSoup) -> DelayTag:
+    products_delayed: DelayTag = {}
+
+    for e in page.findAll('br'):
+        e.extract()
+
+    changed_ps = page.select(
+        ".content > p, .content > center", string=re.compile("発売[月|日]変更")
+    )
+    for c in changed_ps:
+        if 'JAN' in c.get_text():
+            results = re.findall(
+                r"「.*href=\"(.*\/product\/(\d+)\/.*)\".*」JAN：(\d+)", str(c)
+            )
+            for r in results:
+                url = r[0]
+                maker_id = r[1]
+                jan = r[2]
+                products_delayed[maker_id] = {
+                    'url': url,
+                    'jan': jan
+                }
+
+    return products_delayed
